46be3a4d94b7476d729529ae70744e37ebe14058
Nicolas Fenton <brandjamcreative@gmail.com>
2025-05-27 21:07:15 +1000

    added speedinsight and analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/next"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,6 +22,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <SpeedInsights />
+        <Analytics />
+        {/* Main content of the application */}
         {children}
       </body>
     </html>
